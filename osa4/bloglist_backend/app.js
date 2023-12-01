9a957ff688217e70e75df1f119d4a7a71ecc08d2
ellenra <ellen.rajala@gmail.com>
2023-12-01 10:42:48 +0200

    osa4

---
+++ osa4/bloglist_backend/app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.indexOf('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+  }
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ osa4/bloglist_backend/index.js
@@ -0,0 +1,9 @@
+const app = require('./app')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+
+const PORT = 3003
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
